7d8b5d94b5ff1fddaa9a9c818fae3fc7a972b861
Michał Dunat <kontaktmichaldunat@gmail.com>
2022-05-19 14:21:51 +0200

    add initial project

---
+++ src/main/java/com/example/crudreservations/CrudReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudReservationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudreservations/CrudReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReservationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
